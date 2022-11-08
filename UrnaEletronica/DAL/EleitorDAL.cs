d96e2fddb94275d82281e354998de8d6fd8eb918
implementacaoteste <106354515+implementacaoteste@users.noreply.github.com>
2022-11-08 14:39:50 -0300

    Implementação do método alterar
    Implementado o método Alterar na classe EleitorDAL.

--- UrnaEletronica/DAL/EleitorDAL.cs
+++ UrnaEletronica/DAL/EleitorDAL.cs
@@ -47,7 +47,23 @@ namespace DAL
         }
         public void Alterar(Eleitor _eleitor)
         {
-
+            SqlConnection cn = new SqlConnection(Conexao.StringDeConexao);
+            SqlCommand cmd = cn.CreateCommand();
+            try
+            {
+                cmd.CommandText = "UPDATE Eleitor SET Nome = @Nome, Titulo = @Titulo, Votou = @Votou WHERE Id = @Id";
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@Nome", _eleitor.Nome);
+                cmd.Parameters.AddWithValue("@Titulo", _eleitor.Titulo);
+                cmd.Parameters.AddWithValue("@Votou", _eleitor.Votou);
+                cmd.Parameters.AddWithValue("@Id", _eleitor.Id);
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
         public List<Eleitor> Buscar(string _titulo)
         {
