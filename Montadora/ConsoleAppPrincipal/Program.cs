de65b7a99c9abdba53c32299770d8877838277e0
implementacaoteste <106354515+implementacaoteste@users.noreply.github.com>
2023-02-03 19:13:13 -0300

    Reinício do projeto

---
+++ Montadora/ConsoleAppPrincipal/Program.cs
@@ -0,0 +1,18 @@
+﻿using Models;
+using System.Runtime.ConstrainedExecution;
+using System.Text.RegularExpressions;
+
+namespace ConsoleAppPrincipal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pneu pneu1 = new Pneu(16, "Carro de passeio", "Firestone", false, 150);
+            
+            pneu1.Girar(5);
+
+            pneu1.Exibir();
+        }
+    }
+}
---
+++ Montadora/Models/Pneu.cs
@@ -0,0 +1,103 @@
+﻿using System.Runtime.ConstrainedExecution;
+using System.Text.RegularExpressions;
+
+namespace Models
+{
+    public class Pneu
+    {
+        public int Aro { get; set; }
+        public bool TWI { get; set; }
+        public string Cor { get; set; }
+        public string Marca { get; set; }
+        public int VelocidadeMaxima { get; set; }
+        public int VelocidadeAtual { get; set; }
+        public bool Estepe { get; set; }
+        public bool Estourado { get; set; }
+        public bool Furado { get; set; }
+        public string Tipo { get; set; }
+        public int PercentualBorracha { get; set; }
+
+        public Pneu(int _aro, string _tipo, string _marca, bool _estepe, int _velocidadeMaxima)
+        {
+            Aro = _aro;
+            Tipo = _tipo;
+            Marca = _marca;
+            Estepe = _estepe;
+            VelocidadeMaxima = _velocidadeMaxima;
+            Cor = "Preto";
+            Furado = false;
+            Estourado = false;
+            VelocidadeAtual = 0;
+            PercentualBorracha = 100;
+            TWI = false;
+        }
+
+        public void Girar(int _velocidade)
+        {
+            if(Estourado == true)
+            {
+                Console.WriteLine("O pneu está estourado.");
+                return;
+            }
+            if (Furado == true)
+            {
+                Console.WriteLine("O pneu está furado.");
+                return;
+            }
+
+            VelocidadeAtual = VelocidadeAtual + _velocidade;
+            PercentualBorracha = PercentualBorracha - 3;
+
+            if (VelocidadeAtual > VelocidadeMaxima || PercentualBorracha <= 30)
+            {
+                EstourarPneu();
+            }
+        }
+
+        public void Frear(int _reducao)
+        {
+            VelocidadeAtual = VelocidadeAtual - _reducao;
+            PercentualBorracha = PercentualBorracha - 5;
+
+            if (VelocidadeAtual < 0)
+            {
+                VelocidadeAtual = 0;
+            }
+
+            if (PercentualBorracha < 0)
+            {
+                PercentualBorracha = 0;
+            }
+
+            if (PercentualBorracha <= 30)
+            {
+                EstourarPneu();
+            }
+        }
+
+        private void EstourarPneu()
+        {
+            Estourado = true;
+            VelocidadeAtual = 0;
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine("Cor: " + Cor);
+            Console.WriteLine("Tipo: " + Tipo);
+            Console.WriteLine("Marca: " + Marca);
+            Console.WriteLine("Aro: " + Aro);
+            Console.WriteLine("Furado: " + Furado);
+            Console.WriteLine("Estourado: " + Estourado);
+            Console.WriteLine("Estepe: " + Estepe);
+            Console.WriteLine("VelocidadeAtual: " + VelocidadeAtual);
+            Console.WriteLine("VelocidadeMaxima: " + VelocidadeMaxima);
+            Console.WriteLine("PercentualBorracha: " + PercentualBorracha);
+            Console.WriteLine("TWI: " + TWI);
+        }
+    }
+
+}
+
+
+
